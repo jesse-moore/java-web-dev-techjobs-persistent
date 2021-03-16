43d1792ca861757c60075c12cc075979c7168fae
Jesse <moore.jesse@gmail.com>
2021-03-16 09:47:23 -0500

    Part 3

--- queries.sql
+++ queries.sql
@@ -1,6 +1,8 @@
 ## Part 1: Test it with SQL
+## employer: varchar(255), id: int, name: varchar(255), skills: varchar(255)
 
 ## Part 2: Test it with SQL
+SELECT name from employer where location like '%St. Louis City%';
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max=500)
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void getDescription(String description) {
+        this.description = description;
+    }
 }
